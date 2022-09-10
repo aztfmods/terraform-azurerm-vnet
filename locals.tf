816311e6a336f3a0b1649cf881c538046e7aeed8
dkool <dennis.kool@cloudnation.nl>
2022-09-10 13:58:52 +0200

    feat: add optional nsg rules

--- locals.tf
+++ locals.tf
@@ -8,8 +8,10 @@ locals {
         address_prefixes           = subnet.cidr
         rg_name                    = azurerm_resource_group.rg[network_key].name
         subnet_name                = "sn-${var.env}-${network_key}-${subnet_key}"
+        nsg_name                   = "nsg-${var.env}-${network_key}-${subnet_key}"
         location                   = network.location
         endpoints                  = try(subnet.endpoints, [])
+        rules                      = try(subnet.rules, {})
         delegations                = try(subnet.delegations, [])
         virtual_network_name       = azurerm_virtual_network.vnets[network_key].name
         enforce_priv_link_service  = try(subnet.enforce_priv_link_service, false)
