7db7245e4ee82187d1922c70329f2e2fa65f5363
dkool <dennis.kool@cloudnation.nl>
2022-10-28 12:53:57 +0200

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vnets" {
   value = azurerm_virtual_network.vnets
 }
 
+output "subnets" {
+  value = azurerm_subnet.subnets
+}
+
 output "merged_ids" {
   value = concat(values(azurerm_virtual_network.vnets)[*].id, values(azurerm_network_security_group.nsg)[*].id)
 }
