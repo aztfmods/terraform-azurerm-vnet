ca7c21016b652aca2320e7dd738f2af1b5cb9796
dkool <dennis.kool@cloudnation.nl>
2022-12-01 13:33:26 +0100

    feat: trigger workflow

--- main.tf
+++ main.tf
@@ -123,4 +123,6 @@ resource "azurerm_subnet_network_security_group_association" "nsg_as" {
 
   subnet_id                 = azurerm_subnet.subnets[each.key].id
   network_security_group_id = azurerm_network_security_group.nsg[each.key].id
+
+  depends_on = [time_sleep.wait]
 }
