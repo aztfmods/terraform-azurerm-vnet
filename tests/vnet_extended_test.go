a00502e1727fe91e324530a9538a958f38d78e1d
dkool <dennis.kool@cloudnation.nl>
2023-04-29 14:41:39 +0200

    feat: refactor tests to use shared package

--- tests/vnet_extended_test.go
+++ tests/vnet_extended_test.go
@@ -1,10 +1,7 @@
-package tests
+package main
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-03-01/network"
@@ -12,62 +9,15 @@ import (
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/require"
+	"github.com/aztfmods/module-azurerm-vnet/shared"
 )
-
-var filesToCleanup = []string{
-	".terraform",
-	".terraform.lock.hcl",
-	"terraform.tfstate",
-	"terraform.tfstate.backup",
-}
-
-func TestApplyNoError(t *testing.T) {
-	t.Parallel()
-
-	tests := map[string]string{
-		"simple":              "../examples/simple",
-		"ddos-protection":     "../examples/ddos-protection",
-		"diagnostic-settings": "../examples/diagnostic-settings",
-		"nsg-rules":           "../examples/nsg-rules",
-		"service-endpoints":   "../examples/service-endpoints",
-		"delegations":         "../examples/delegations",
-	}
-
-	for name, path := range tests {
-		t.Run(name, func(t *testing.T) {
-			terraformOptions := &terraform.Options{
-				TerraformDir: path,
-				NoColor:      true,
-				Parallelism:  2,
-			}
-
-			terraform.WithDefaultRetryableErrors(t, &terraform.Options{})
-
-			defer cleanupFiles(path)
-			defer func() {
-				sequentialDestroy(t, terraformOptions)
-				cleanupFiles(path)
-			}()
-
-			terraform.InitAndApply(t, terraformOptions)
-		})
-	}
-}
 
 func TestVirtualNetwork(t *testing.T) {
 	t.Parallel()
 
-	tfOpts := &terraform.Options{
-		TerraformDir: "../examples/complete",
-		NoColor:      true,
-		Parallelism:  20,
-	}
+	tfOpts := shared.GetTerraformOptions("../examples/complete")
 
-	defer func() {
-		sequentialDestroy(t, tfOpts)
-		cleanupFiles("../examples/complete")
-	}()
-
+	defer shared.Cleanup(t, tfOpts)
 	terraform.InitAndApply(t, tfOpts)
 
 	vnet := terraform.OutputMap(t, tfOpts, "vnet")
@@ -134,18 +84,3 @@ func verifySubnetsExist(t *testing.T, subscriptionID string, resourceGroupName s
 	}
 }
 
-func cleanupFiles(dir string) {
-	for _, file := range filesToCleanup {
-		filePath := filepath.Join(dir, file)
-		fmt.Printf("Cleaning up %s\n", file)
-		if err := os.RemoveAll(filePath); err != nil {
-			fmt.Printf("Failed to remove %s: %v\n", filePath, err)
-		}
-	}
-}
-
-// api limitations https://github.com/hashicorp/terraform-provider-azurerm/issues/17565
-func sequentialDestroy(t *testing.T, tfOpts *terraform.Options) {
-	tfOpts.Parallelism = 1
-	terraform.Destroy(t, tfOpts)
-}
