83803ba266ba0f4b70e9a930de33ca693a814d20
dkool <dennis.kool@cloudnation.nl>
2023-07-04 12:28:59 +0200

    feat: fix more linting issues

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "rg" {
-  source = "github.com/aztfmods/terraform-azure-rg"
+  source = "github.com/aztfmods/terraform-azure-rg?ref=1.0.0"
 
   environment = var.environment
   suffix      = "001"
--- examples/delegations/main.tf
+++ examples/delegations/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "rg" {
-  source = "github.com/aztfmods/terraform-azure-rg"
+  source = "github.com/aztfmods/terraform-azure-rg?ref=v1.0.0"
 
   environment = var.environment
 
