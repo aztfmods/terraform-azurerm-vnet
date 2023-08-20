2bba19e3a353b97a238149fcf27cc7b873e71240
dkool <dennis.kool@cloudnation.nl>
2023-08-20 10:38:31 +0200

    cleanup

--- examples/multiple/locals.tf
+++ examples/multiple/locals.tf
@@ -43,7 +43,7 @@ locals {
 
 locals {
   naming = {
-    # lookup outputs from the naming module
+    # lookup outputs to have consistent naming
     for type in local.naming_types : type => lookup(module.naming, type).name
   }
 
